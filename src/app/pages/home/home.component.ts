83613e314cdc2743be2033d2c690381299530ab9
Michael Koch <m.koch@neuko-devment.com>
2025-04-20 16:24:46 +0200

    Add Story component with template, styles, and tests; update Home component to include Story

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AdventuresComponent } from "./components/adventures/adventures.component";
+import { StoryComponent } from "./components/story/story.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AdventuresComponent],
+  imports: [AdventuresComponent, StoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
