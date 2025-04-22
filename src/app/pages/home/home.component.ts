c0a605a9e2e5dfac1632f1059e7784acecfb4e40
Michael Koch <m.koch@neuko-devment.com>
2025-04-22 08:49:33 +0200

    feat(parts): add PartsComponent with initial test suite
    - Created PartsComponent with basic structure and empty implementation.
    - Added unit tests for PartsComponent to ensure it is created successfully.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AdventuresComponent } from "./components/adventures/adventures.component";
 import { StoryComponent } from "./components/story/story.component";
+import { PartsComponent } from "./components/parts/parts.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AdventuresComponent, StoryComponent],
+  imports: [AdventuresComponent, StoryComponent, PartsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
