0eaff3b38fd1f1c82e69896feec7478bca8013e1
Michael Koch <m.koch@neuko-devment.com>
2025-06-12 11:42:57 +0200

    feat(guide): add GuideComponent with template, styles, and tests; update Home component to include Guide

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AdventuresComponent } from "./components/adventures/adventures.component";
 import { StoryComponent } from "./components/story/story.component";
 import { PartsComponent } from "./components/parts/parts.component";
+import { GuideComponent } from "./components/guid/guide.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AdventuresComponent, StoryComponent, PartsComponent],
+  imports: [AdventuresComponent, StoryComponent, PartsComponent, GuideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
